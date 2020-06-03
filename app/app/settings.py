492544fb876d043f597dc2f93d222d316ed7bb54
Anil <anilreddy864@gmail.com>
2020-06-03 17:53:31 +0530

    Added UserProfile Model

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'profiles_api',
 ]
 
@@ -115,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
