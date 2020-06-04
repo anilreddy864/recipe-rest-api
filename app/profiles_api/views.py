40093872fb74bcec76e192af3a2b3843ae6b4d8f
Anil <anilreddy864@gmail.com>
2020-06-04 11:46:57 +0530

    Added Simple Hello View API GET

--- app/profiles_api/views.py
+++ app/profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return features"""
+        api_list = ['Uses HTTP Methods',
+                    'Django View',
+                    'Mapped Manually to URLs']
+        return Response({'message': 'Hello', 'api_view': api_list})
